ca2757db5ca929a5e4062ee3752206b51242e388
KZZahariev <dsad_1993@abv.bg>
2023-12-18 14:16:32 +0200

    update dbUrl

--- Rest-api/config/config.js
+++ Rest-api/config/config.js
@@ -8,7 +8,7 @@ const config = {
     },
     production: {
         port: process.env.PORT || 3000,
-        dbURL: 'mongodb://127.0.0.1:27017/Travel-in-Bulgaria-react', //'mongodb+srv://KZZahariev:Paris9602@travel-in-bulgaria.0tvahgf.mongodb.net/?retryWrites=true&w=majority', //process.env.DB_URL_CREDENTIALS, //
+        dbURL: 'mongodb+srv://KZZahariev:Paris9602@travel-in-bulgaria.0tvahgf.mongodb.net/?retryWrites=true&w=majority', //process.env.DB_URL_CREDENTIALS, //
         origin: ['https://travel-in-bulgaria-react-app-kbx9.vercel.app'] //'http://localhost:5555', 'http://localhost:4200',
     }
 };
